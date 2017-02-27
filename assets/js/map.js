42d78dced3704e225a0192e00d73fceaa67a6202
Vesper <veswill3@gmail.com>
2017-02-27 14:19:58 +0700

    update expired mapbox api access token

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,7 +6,7 @@
         zoom: 2,
         maxBounds: L.latLngBounds([-90, -180], [90, 180])
     });
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidmVzd2lsbDMiLCJhIjoiY2lqZDJ3OHVyMDAzdnYxbTBuNmhneDVvdSJ9.hoP6Xpo2MeGLnMHd7p8_sw', {
         id: 'mapbox.emerald',
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         minZoom: 2
